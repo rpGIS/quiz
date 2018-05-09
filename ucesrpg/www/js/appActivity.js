ddd9380d0b766c5ea408488976c9c2fc3f72edd9
rpGIS <rpgiesler@gmail.com>
2018-05-09 10:50:54 -0400

    bug fix

--- ucesrpg/www/js/appActivity.js
+++ ucesrpg/www/js/appActivity.js
@@ -26,7 +26,7 @@ function loadquestionlayer(questiondata) {
   var questionlayer = L.geoJson(questionjson,
   {
   onEachFeature: function (feature, layer) {
-    layer.bindPopup(feature.properties.question+'<br> <form onsubmit="return (getScore()&& startDataUpload());"> <input type="radio" name="answer", id="1", value="Answer1">'+feature.properties.answer1+ '<br> <input type="radio" name="answer", id="2" value="Answer2">'+feature.properties.answer2+ '<br> <input type="radio" name="answer", id="3", value="Answer3">'+feature.properties.answer3+ '<br> <input type="radio" name="answer", id="4", value="Answer4">' + feature.properties.answer4 +'<br> <input type="submit",name="submit", id="submit", value="Submit"> <input id="hidden" name="hidden" type="hidden" value='+feature.properties.correct+'><input id="question" name="question" type="hidden" value='+feature.properties.question+'></form>');
+    layer.bindPopup(feature.properties.question+'<br> <form onsubmit="return (getScore()&& startDataUpload());"> <input type="radio" name="answer", id="1", value="Answer1">'+feature.properties.answer1+ '<br> <input type="radio" name="answer", id="2" value="Answer2">'+feature.properties.answer2+ '<br> <input type="radio" name="answer", id="3", value="Answer3">'+feature.properties.answer3+ '<br> <input type="radio" name="answer", id="4", value="Answer4">' + feature.properties.answer4 +'<br> <input type="submit",name="submit", id="submit", value="Submit"> <input id="hidden" name="hidden" type="hidden" value='+feature.properties.correct+'><input id="question" name="question" type="hidden" value="'+feature.properties.question+'"></form>');
           },
 
     // use point to layer to create the points
